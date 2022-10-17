2d0a7cec0a67b12f75e506f8cd43d72e957df3db
el-armani <benjaminozor247@gmail.com>
2022-10-17 09:20:31 -0700

    C intro 0x01 adv task 1

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - print 0 to 99 without any dilucated combos
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int ones;
+	int tens;
+
+	for (tens = '0'; tens <= '9'; tens++) /*increment tens*/
+	{
+		for (ones = (tens + 1); ones <= '9'; ones++) /*one's ten + 1*/
+		{
+			putchar(tens);
+			putchar(ones);
+
+			if (tens != '8' || ones != '9') /*print commas*/
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
