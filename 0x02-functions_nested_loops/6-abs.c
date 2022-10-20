da98f2c5f67e7366d8dcae03dcffd59007a69b02
el-armani <benjaminozor247@gmail.com>
2022-10-19 18:33:21 -0700

    C intro 0x02 task 6 commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of a number
+ * @v: value to examine
+ * Return: absolute value of v
+ */
+int _abs(int v)
+{
+	if (v > 0)
+		return (n);
+	else
+		return (-n);
+}
