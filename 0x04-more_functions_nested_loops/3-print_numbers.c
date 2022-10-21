f2a8c9df0f58d38160f7a42dcb2dab729c3d1024
el-armani <benjaminozor247@gmail.com>
2022-10-21 11:12:01 -0700

    C intro 0x04 task 3 commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers 0 through 9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+		_putchar(num);
+	_putchar("\n");
+}
