252ccf4a2dd859b0b094614a51d8e738179ec8db
el-armani <benjaminozor247@gmail.com>
2022-10-24 12:18:43 -0700

    C intro 0x05 task 0 commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * reset_to_98 - resets value on main to 98
+ * @n: value to be reset 
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+projects on this directory, are projects on loops, pointers arrays and strings, just as the directory name implpies 
